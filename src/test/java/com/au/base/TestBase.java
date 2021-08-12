a2e89bbbcc81aa0ab5e6b6a48a0886d11219a8aa
LikhithKumar <likhithjune@gmail.com>
2021-08-12 14:16:20 +0530

    done

--- src/test/java/com/au/base/TestBase.java
+++ src/test/java/com/au/base/TestBase.java
@@ -3,6 +3,7 @@ package com.au.base;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
@@ -19,19 +20,24 @@ public class TestBase {
 	public void setUp() throws IOException {
 		if (driver == null) {
 			fis = new FileInputStream(System.getProperty("user.dir")
-					+ "\\DataDriven\\src\\test\\resources\\properties\\Config.properties");
+					+ "\\src\\test\\resources\\properties\\Config.properties");
 			config.load(fis);
 			fis = new FileInputStream(
 					System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
 			or.load(fis);
 
 			if (config.getProperty("browser").equals("chrome")) {
-				System.setProperty("webdriver.vhrome.driver", System.getProperty("user.dir")+"\\src\\test\\executables\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\src\\test\\executables\\chromedriver.exe");
 
 			} else if (config.getProperty("browser").equals("firefox")) {
-				System.setProperty("webdriver.vhrome.driver", "chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
 			}
+			driver.get(config.getProperty("testsiteurl"));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")),
+					TimeUnit.SECONDS);
 
 		}
 
@@ -39,6 +45,8 @@ public class TestBase {
 
 	@AfterSuite
 	public void tearDown() {
-
+if(driver!=null) {
+	driver.quit();
+}
 	}
 }
--- src/test/java/com/au/roughtest/TestProperties.java
+++ src/test/java/com/au/roughtest/TestProperties.java
@@ -1,7 +1,6 @@
 package com.au.roughtest;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
